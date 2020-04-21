71c17b2dade8532ad41f87bb89fea120e3f27f88
Taras Halturin <halturin@gmail.com>
2020-04-22 01:52:08 +0200

    erlang ping done

--- ping_test.erl
+++ ping_test.erl
@@ -1,38 +1,67 @@
--module(ping).
+-module(ping_test).
 
--export([start_ping/1]).
+-export([start_ping/2]).
 
 
 
-start_ping(N) -> 
+start_ping(N, V) ->
+    spawn(fun() ->
     RemotePids = gen_server:call({pong_server, 'erl-demo@127.0.0.1'}, {start, N}),
-    LocalPids = [spawn(fun() -> ping(number, P, self()) end) || P <- RemotePids],
-    handleReport(LocalPids).
+    ReportTo = self(),
+    LocalPids = [spawn(fun() -> ping(V, P, ReportTo, 0, erlang:timestamp()) end) || P <- RemotePids],
+    handleReport(LocalPids, [])
+          end).
 
-handleReport(L) ->
+handleReport([], R) ->
+    {pong_server, 'erl-demo@127.0.0.1'} ! stop,
+    Result = lists:foldl(fun({_,X}, Sum) -> X + Sum end, 0, R),
+    io:format("done. RPS = ~p~n", [Result/10]);
+
+handleReport(L, R) ->
     receive
-        {report, X} ->
-            
+        {report, T, N, P} ->
+            R1 = [{T,N}| R],
+            L1 = lists:filter(fun(X) -> X /= P end, L),
+            handleReport(L1, R1);
 
-            handleReport(L);
+        {failed, P} ->
+            io:format("failed result from ~p~n", [P]),
+            L1 = lists:filter(fun(X) -> X /= P end, L),
+            handleReport(L1, R);
+
         Unknown ->
-            io:format("unknown report ~p~n", Unknown)
-            handleReport(L)
+            io:format("unknown report ~p~n", Unknown),
+            handleReport(L, R)
 
     after 50000 ->
           io:format("timeout. shutting down~n"),
           {pong_server, 'erl-demo@127.0.0.1'} ! stop
     end. 
 
-ping(number, P, ReportTo) ->
-    ping(12345, P, ReportTo);
-ping(string, P, ReportTo) ->
-    ping("hello world", P, ReportTo);
-ping(pid, P, ReportTo) ->
-    ping(self(), P, ReportTo);
-ping(list, P, ReportTo) ->
+ping(number, P, ReportTo, N, StartedAt) ->
+    ping(12345, P, ReportTo, N, StartedAt);
+ping(string, P, ReportTo, N, StartedAt) ->
+    ping("hello world", P, ReportTo, N, StartedAt);
+ping(pid, P, ReportTo, N, StartedAt) ->
+    ping(self(), P, ReportTo, N, StartedAt);
+ping(list, P, ReportTo, N, StartedAt) ->
     L = [self() || _ <- lists:seq(1,100)],
-    ping(L, P, ReportTo);
+    ping(L, P, ReportTo, N, StartedAt);
 
-ping(Message, P, ReportTo) ->
+ping(Message, P, ReportTo, N, StartedAt) ->
+    Ref = make_ref(),
+    P ! {'$gen_call', {self(), Ref}, Message},
+    receive
+        {Ref, ok} ->
+            T = timer:now_diff(erlang:timestamp(), StartedAt),
+            % run 10 seconds 
+            if T > 10000000 ->
+                ReportTo ! {report, T, N, self()};
+            true ->
+                ping(Message, P, ReportTo, N+1, StartedAt)
+            end       
+
+    after 5000 ->
+         ReportTo ! {failed, self()}
+    end.
 
